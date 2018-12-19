a37d376066f50e03a54de8e981b6a49ab7e23dc5
Juan Pérez-Sorrosal <jperezsorrosal@gmail.com>
2018-12-19 23:59:23 +0100

    Manual Glue

--- playground/src/main/scala/Glue.scala
+++ playground/src/main/scala/Glue.scala
@@ -1,6 +1,20 @@
+
+
 object Glue extends App {
-  def f(a: Int): Int = a * 2
-  def g(a: Int): Int = a * 3
-  val x = g(f(100))
-  println(x)
+
+  def f(a: Int): (Int, String) = {
+    val result = a * 2
+    (result, s"\nf result: $result.")
+  }
+  def g(a: Int): (Int, String) = {
+    val result = a * 3
+    (result, s"\ng result: $result.")
+  }
+  // get the output of `f`
+  val (fInt, fString) = f(100)
+  // plug the Int from `f` as the input to `g`
+  val (gInt, gString) = g(fInt)
+  // create the total "debug string" by manually merging // the strings from f and g
+  val debug = fString + " " + gString
+  println(s"result: $gInt, debug: $debug")
 }
