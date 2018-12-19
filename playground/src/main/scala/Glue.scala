2d4beca13fcec891b73aa97738a07db5d6582ef9
Juan Pérez-Sorrosal <jperezsorrosal@gmail.com>
2018-12-20 00:47:48 +0100

    Using bind function to bind functions

--- playground/src/main/scala/Glue.scala
+++ playground/src/main/scala/Glue.scala
@@ -2,19 +2,24 @@
 
 object Glue extends App {
 
-  def f(a: Int): (Int, String) = {
-    val result = a * 2
-    (result, s"\nf result: $result.")
+  def f(a: Int): (Int, String) = (a, s"f($a)=$a")
+  def g(a: Int): (Int, String) = (2*a, s"g($a)=2*$a=${2*a}")
+  def h(a: Int): (Int, String) = (2*a, s"h($a)=3*$a=${3*a}")
+
+
+  val fResult = f(100)
+  val gResult = bind(g, fResult)
+  val hResult = bind(h, gResult)
+
+  printResult(hResult)
+
+  def printResult(t: (Int, String)): Unit = println(s"Result: ${t._1}, debug message: '${t._2}'")
+
+  def bind(f: Int => (Int, String), tuple: (Int, String)): (Int, String) = {
+    val res = f(tuple._1)
+
+    (res._1, tuple._2 + '\n' + res._2 )
   }
-  def g(a: Int): (Int, String) = {
-    val result = a * 3
-    (result, s"\ng result: $result.")
-  }
-  // get the output of `f`
-  val (fInt, fString) = f(100)
-  // plug the Int from `f` as the input to `g`
-  val (gInt, gString) = g(fInt)
-  // create the total "debug string" by manually merging // the strings from f and g
-  val debug = fString + " " + gString
-  println(s"result: $gInt, debug: $debug")
+
+
 }
